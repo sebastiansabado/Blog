ba6a56d67929edff03572a105dc0ac12fc432f35
sebastiansabado <sebastiansabado2@yahoo.com>
2017-09-06 09:05:15 -0700

    Nav Bar linking

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,9 @@
  <div class="blog-masthead">
       <div class="container">
         <nav class="nav blog-nav">
-          <a class="nav-link active" href="#">Home</a>
-          <a class="nav-link" href="#">New features</a>
-          <a class="nav-link" href="#">Press</a>
+          <a class="nav-link active" href="{{ url('/') }}">Home</a>
+          <a class="nav-link" href="#">Stats</a>
+          <a class="nav-link" href="#">Owner Stats</a>
           <a class="nav-link" href="#">New hires</a>
           <a class="nav-link" href="#">About</a>
 
